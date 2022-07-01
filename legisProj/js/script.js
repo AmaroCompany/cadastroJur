4a18b1c27f49dcd24506454d7327f200129a4dae
thiagoamaro <thiagoamaroc@gmail.com>
2022-07-01 16:36:13 -0300

    Add files via upload

--- legisProj/js/script.js
+++ legisProj/js/script.js
@@ -1,25 +1,21 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+//classes guardar dados
+const id = obj.id;
+const leiN = obj.leiN;
+const artigoN = obj.artigoN;
+const incisoN = obj.incisoN;
+const paragrafoN = "$" + obj.paragrafoN;
+let redacao = obj.redacao;
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCYY91YQo5QTriyzhjt7d_UqSpY7Fod8jM",
-    authDomain: "proclegis-810dc.firebaseapp.com",
-    projectId: "proclegis-810dc",
-    storageBucket: "proclegis-810dc.appspot.com",
-    messagingSenderId: "664177340102",
-    appId: "1:664177340102:web:f49bd784130dfbe806b1c7",
-    measurementId: "G-G27T970E8N"
-  };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+const artigo = [id, leiN, artigoN, incisoN, paragrafoN, redacao];
+
+
+//buscando dados
 
 
 
-//classes para requisição do formulário
+//botão para salvar dados
+
+function Salvar(artigo[]){
+   
+}
