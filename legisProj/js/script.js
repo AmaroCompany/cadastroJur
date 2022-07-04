e1a3f4b1d22f04c5ba0247187f4f3decbf340407
thiagoamaro <thiagoamaroc@gmail.com>
2022-07-04 16:47:43 -0300

    Add files via upload

--- legisProj/js/script.js
+++ legisProj/js/script.js
@@ -1,21 +1,35 @@
-//classes guardar dados
-const id = obj.id;
-const leiN = obj.leiN;
-const artigoN = obj.artigoN;
-const incisoN = obj.incisoN;
-const paragrafoN = "$" + obj.paragrafoN;
-let redacao = obj.redacao;
+//ESTRUTURA DADOS
+//Lei
+//lei {id:12345, nome:"loremIpsum"};
+const lei = obj.lei;
+const lei String = obj.lei;
+
+//Artigo
+//artigo {lei:12345, numeroArt:1, redacaoArt:"loremIpsum"};
+const artLei = obj.artLei;
+const art = obj.art;
+const artRedacao String = obj.artRedacao;
+
+//Parágrafo
+//paragrafo {lei:12345, numeroArt:1, numeroParagrafo:1, redacaoPar:"LoremIpsum"};
+const paragLei = obj.paragLei;
+const paragArt = obj.paragArt;
+const paragrafo = obj.paragrafo;
+const redacaoParag = obj.redacaoParag;
 
 
-const artigo = [id, leiN, artigoN, incisoN, paragrafoN, redacao];
+//Inciso
+//inciso {lei:12345, numArt:1, numParag:1, numInciso:1, redacaoIn:"loremIpsum"};
+const incLei = obj.incLei;
+const incArt = obj.incArt;
+const incParag = obj.incParag;
+const inciso = obj.inciso;
+const redacaoInc = obj.redacaoInc;
 
 
-//buscando dados
-
-
-
-//botão para salvar dados
-
-function Salvar(artigo[]){
-   
+//Seletor de Leis
+function changeTheme() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
+//
